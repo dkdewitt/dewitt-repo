73ea2430e7c05ea55f7fe36c9d16a8e39d9ece10
dkdewitt <dkdewitt@gmail.com>
2013-10-15 09:26:19 -0700

    first commit

---
+++ stuffsystem/ejb/src/main/java/com/stuffsystem/service/InventoryServiceBean.java
@@ -0,0 +1,33 @@
+package com.stuffsystem.service;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+@TransactionManagement(TransactionManagementType.CONTAINER) //default
+//@TransactionManagement(TransactionManagementType.BEAN) // to manage it here
+public class InventoryServiceBean { 
+
+    @PersistenceContext(unitName = "primary")
+    private EntityManager em;
+
+    // ctor
+    public InventoryServiceBean() {
+    }
+
+    ///// Get the item details if you only have an item ID.
+    //@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    //public Item getBookDetails(Long itemId) {
+    //    //Item item = em.find(Item.class, itemId);
+    //    Query q = em.createQuery("select i from Item i where i.id = " + itemID);
+    //    List<Item> results = q.getResultList();
+    //    Item item = null;
+    //    if(results != null && results.size() > 0) {
+    //        item = results.get(0);
+    //    }
+    //    return item;
+    //}
+}
